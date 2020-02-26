674c666ccbcc830c0f21d8763f1161ba5eefb28b
leyla li <leylali@qq.com>
2020-02-26 10:22:46 -0800

    done with scroll up animation

--- main.js
+++ main.js
@@ -10,15 +10,15 @@ $(document).ready(function(){
     });
 
     $('nav a[href*="#"]').on('click', function(){
-        
-        $('html body').animate({
-            scrollTop: $($(this).attr('href')).offset().top - 100}, 2000);
-        
-        // $('.top-nav').removeClass('open');
+        console.log($(this));
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000);
     });
 
     $('#up').on('click', function(){
-        $('html body').animate({
-            scrollTop: 0}, 2000);
+        $('html, body').animate({
+            scrollTop: 0
+        }, 2000);
     });
 });
